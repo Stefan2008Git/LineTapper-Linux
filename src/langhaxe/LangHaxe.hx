5ce5c8b7e254236e7e3ef371bc4ecbb7ceb11d4f
IdealistCat <mattallan.h@gmail.com>
2024-12-25 01:34:55 -0600

    getPhrase switch statement for every phrase.

--- src/langhaxe/LangHaxe.hx
+++ src/langhaxe/LangHaxe.hx
@@ -21,29 +21,50 @@ typedef PhrasesJson = {
 	// song select menu
 	var ?start_typing_your_songs_name:String;
     // settings menu
-	var ?settings_Messages:Array<String>;
-	var ?search_Settings:String;
+	var ?settings_messages:Array<String>;
+	var ?search_settings:String;
 	var ?settings:String;
-	var ?settings_Graphics:String;
-	var ?settings_Antialiasing:String;
-	var ?settings_Gameplay:String;
-	var ?settings_Tile_Offset:String;
-	var ?settings_Audio:String;
-	var ?settings_Master_Volume:String;
-	var ?settings_Music_Volume:String;
-	var ?settings_SFX_Volume:String;
+	var ?settings_graphics:String;
+	var ?settings_antialiasing:String;
+	var ?settings_gameplay:String;
+	var ?settings_tile_offset:String;
+	var ?settings_audio:String;
+	var ?settings_master_volume:String;
+	var ?settings_music_volume:String;
+	var ?settings_sfx_volume:String;
 }
 
 class PhraseManager
 {
 
-    public static function getPhrase(phrase:String)
+    public static function getPhrase(phrase:String, json:PhrasesJson):Dynamic
     {
 
-        switch(phrase.toLowerCase())
-        {
+        switch(phrase.toLowerCase().replace(' ', '_'))
+		{
+			case 'beats_per_minute': return json.beats_per_minute;
+			case 'credits': return json.credits;
+			case 'gameplay': return json.gameplay;
+			case 'level_editor': return json.level_editor;
+			case 'linetapper': return json.linetapper;
+			case 'mainmenu': return json.mainmenu;
+			case 'search_settings': return json.search_settings;
+			case 'settings': return json.settings;
+			case 'settings_antialiasing': return json.settings_antialiasing;
+			case 'settings_audio': return json.settings_audio;
+			case 'settings_gameplay': return json.settings_gameplay;
+			case 'settings_graphics': return json.settings_graphics;
+			case 'settings_master_volume': return json.settings_master_volume;
+			case 'settings_messages': return json.settings_messages;
+			case 'settings_music_volume': return json.settings_music_volume;
+			case 'settings_sfx_volume': return json.settings_sfx_volume;
+			case 'settings_tile_offset': return json.settings_tile_offset;
+			case 'song_select': return json.song_select;
+			case 'start_typing_your_songs_name': return json.start_typing_your_songs_name;
+
             default:
-                trace('Unknown phrase: $phrase');
+                trace('[PHRASE MANAGER] Unknown phrase: $phrase');
+                return phrase;
         }
         
     }
