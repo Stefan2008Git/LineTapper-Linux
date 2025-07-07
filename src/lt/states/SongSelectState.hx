68293b6497a6bb3558e885c3bc6dd8fcb2fb715f
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-07 10:57:57 -0500

    Fix build flag and add a text

--- src/lt/states/SongSelectState.hx
+++ src/lt/states/SongSelectState.hx
@@ -1,3 +1,15 @@
 package lt.states;
 
-class SongSelectState extends State {}
+class SongSelectState extends State {
+	var topText:FlxText;
+        var topText_phrase:String = 'SELECT A SONG';
+
+	override function create() {
+		super.create();
+
+		topText = new FlxText(20, 180, -1, PhraseManager.getPhrase(topText_phrase, topText_phrase), 20);
+		topText.setFormat(Assets.font("extenro-extrabold"), 22, FlxColor.WHITE, CENTER, OUTLINE, FlxColor.BLACK);
+		topText.screenCenter(X);
+		add(topText);
+	}
+}
