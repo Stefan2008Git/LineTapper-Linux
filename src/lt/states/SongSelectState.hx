8dac162e65aae233656600e0a13cef7895cdd36a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-07 11:10:31 -0500

    Song text and song switching

--- src/lt/states/SongSelectState.hx
+++ src/lt/states/SongSelectState.hx
@@ -1,15 +1,59 @@
 package lt.states;
 
+import sys.FileSystem;
+
 class SongSelectState extends State {
 	var topText:FlxText;
-        var topText_phrase:String = 'SELECT A SONG';
+	var topText_phrase:String = 'SELECT A SONG';
+
+	var centerText:FlxText;
+
+	var song:String = 'Tutorial';
+	var songList:Array<String> = ['Tutorial'];
+	var songIndex:Int = 0;
 
 	override function create() {
 		super.create();
 
 		topText = new FlxText(20, 180, -1, PhraseManager.getPhrase(topText_phrase, topText_phrase), 20);
-		topText.setFormat(Assets.font("extenro-extrabold"), 22, FlxColor.WHITE, CENTER, OUTLINE, FlxColor.BLACK);
+		topText.setFormat(Assets.font('extenro-extrabold'), 22, FlxColor.WHITE, CENTER, OUTLINE, FlxColor.BLACK);
 		topText.screenCenter(X);
 		add(topText);
+
+		centerText = new FlxText(20, 180, -1, song, 20);
+		centerText.setFormat(Assets.font('extenro-extrabold'), 22, FlxColor.WHITE, CENTER, OUTLINE, FlxColor.BLACK);
+		add(centerText);
+
+		songList = FileSystem.readDirectory('assets/data/maps');
+
+		if (songList.length < 1) {
+			songList = ['Tutorial'];
+		}
+		trace(songList);
+
+		songIndex = songList.indexOf('Tutorial');
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+
+		song = songList[songIndex];
+		centerText.text = song.toLowerCase();
+		centerText.screenCenter();
+
+		if (FlxG.keys.justReleased.LEFT)
+			incrementSongIndex(-1);
+		if (FlxG.keys.justReleased.RIGHT)
+			incrementSongIndex(1);
+	}
+
+	function incrementSongIndex(increment:Int = 0) {
+		songIndex += increment;
+
+		if (songIndex < 0)
+			songIndex = 0;
+
+		if (songIndex >= songList.length - 1)
+			songIndex = songList.length - 1;
 	}
 }
